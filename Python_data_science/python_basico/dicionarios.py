8c879db5c61ba4a23834583ead75d20b153ac3fa
Lucas Vinicius <lucasvinij@gmail.com>
2022-09-26 19:45:50 -0300

    add files

---
+++ Python_data_science/python_basico/dicionarios.py
@@ -0,0 +1,23 @@
+# dicionarios = {}
+
+# pessoa = {"nome":"Guilherme", "idade": 28}
+
+# pessoa = dict(nome = "Guilherme", idade=28)
+
+# pessoa["telefone"] =  "3339-2349"
+
+# contatos = {
+#     "guilherme@gmail.com": {"nome":"Guilherme", "telefone":"33392349"},
+#     "giovana@gmail.com": {"nome":"Giovana", "telefone":"3332-2334"},
+# }
+
+# for chave in contatos:
+#     print(chave, contatos[chave])
+
+
+# contatos.clear()
+
+# contatos.copy()
+
+# dict.fromkeys()
+
